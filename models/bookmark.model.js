c970bdab68906115eedf3036a6567e0280fc22f6
Mojammel Noman <molla.noman23@gmail.com>
2024-11-11 16:20:37 +0600

    add bookmark schema

--- models/bookmark.model.js
+++ models/bookmark.model.js
@@ -1,14 +1,8 @@
 const { Schema, model } = require("mongoose");
 
 const bookmarkSchema = new Schema({
-  title: { type: String, required: true },
-  category: { type: String, required: true },
-  imgURL: { type: String, required: true },
-  content: { type: String, required: true },
-  writerName: { type: String, required: true },
-  writerEmail: { type: String, required: true },
-  userName: { type: String, required: true },
-  userEmail: { type: String, required: true },
+  blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
+  user: { type: String, required: true },
 });
 
 const Bookmark = model("Bookmark", bookmarkSchema);
